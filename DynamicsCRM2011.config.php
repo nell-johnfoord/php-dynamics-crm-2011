97d8aea622f0373df93133779b6213ecbb43c0af
Joarkeryon-JF <114714318+Joarkeryon-JF@users.noreply.github.com>
2022-10-26 12:53:09 +0800

    Update DynamicsCRM2011.config.php

--- DynamicsCRM2011.config.php
+++ DynamicsCRM2011.config.php
@@ -2,7 +2,8 @@
 /* Basic configuation details */
 // Discovery Service URI -> Get this from the Customizations / Developer Resources section of the Microsoft Dynamics CRM 2011 application
 //$discoveryServiceURI = 'https://<your.domain.name>/XRMServices/2011/Discovery.svc';
-$discoveryServiceURI = 'https://johnfoordpteltd.crm5.dynamics.com/XRMServices/2011/Discovery.svc';
+//$discoveryServiceURI = 'https://johnfoordpteltd.crm5.dynamics.com/XRMServices/2011/Discovery.svc';
+$discoveryServiceURI = 'https://disco.crm5.dynamics.com/XRMServices/2011/Discovery.svc';
 // Organization Unique Name -> Get this from the Customizations / Developer Resources section of the Microsoft Dynamics CRM 2011 application
 $organizationUniqueName = 'johnfoordpteltddev';
 // Username & Password - Note that despite the text on the ADFS site, the Domain is NOT required!
