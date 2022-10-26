be1a5a1c11aacf16ba246e748b88cfa7f50f4a07
Joarkeryon-JF <114714318+Joarkeryon-JF@users.noreply.github.com>
2022-10-26 13:45:41 +0800

    Update DynamicsCRM2011_Connector.class.php

--- DynamicsCRM2011_Connector.class.php
+++ DynamicsCRM2011_Connector.class.php
@@ -105,6 +105,14 @@ class DynamicsCRM2011_Connector extends DynamicsCRM2011 {
 	 * @param boolean $_debug display debug information when accessing the server - not recommended in Production!
 	 * @return DynamicsCRM2011Connector
 	 */
+	
+	 @param string $_discoveryURI = 'https://disco.crm5.dynamics.com/XRMServices/2011/Discovery.svc';
+	 @param string $_organizationUniqueName = 'johnfoordpteltddev';
+	 @param string $_username = 'DevUser@johnfoord.com';
+	 @param string $_password = 'Fat39622';
+	 @param boolean $_debug = 'FALSE';
+	 @return DynamicsCRM2011Connector
+	
 	function __construct($_discoveryURI, $_organizationUniqueName = NULL, $_username = NULL, $_password = NULL, $_debug = FALSE) {
 		/* Enable or disable debug mode */
 		self::$debugMode = $_debug;
